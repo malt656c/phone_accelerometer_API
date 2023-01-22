526b5be505a4aa2b8883f9142255f805e3432733
Malte Jensen <malt656c@stud.kea.dk>
2023-01-22 23:46:49 +0100

    sg

--- script.js
+++ script.js
@@ -9,17 +9,14 @@ const maxY = arena.clientHeight - player.clientHeight;
 
 console.log(maxX, maxY)
 function orientering(event) {
-  const x = event.beta;
-  const y = event.gamma;
+  const x = event.beta+90;
+  const y = event.gamma+90;
 
- x_dir.textContent = `x : ${x.toFixed(2)}`;
- y_dir.textContent = `y : ${y.toFixed(2)}`;
+  player.style.top = `${(maxX * x) / 90 - 10}px`;
+  player.style.left = `${(maxY * y) / 360 - 10}px`;
 
-  x += 90;
-  y += 90;
-
-  player.style.top = `${(maxY * y) / 180 - 10}px`;
-  player.style.left = `${(maxX * x) / 180 - 10}px`;
+   x_dir.textContent = `x : ${x.toFixed(2)} ${maxX}`;
+ y_dir.textContent = `y : ${y.toFixed(2)} ${maxY}`;
 }
 
 window.addEventListener("deviceorientation", orientering);
