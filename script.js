cc82cea83521772f387454c1c0ab82d1194f7b60
Malte Jensen <malt656c@stud.kea.dk>
2023-01-22 21:55:06 +0100

    test

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const z_dir = document.querySelector(".z-direction");
+
 const x_dir = document.querySelector(".x-direction");
 const y_dir = document.querySelector(".y-direction");
+const arena = document.querySelector(".arena");
+const player = document.querySelector(".player");
+
+const maxX = arena.clientWidth - player.clientWidth;
+const maxY = arena.clientHeight - player.clientHeight;
 
 function orientering(event) {
-  const z = event.alpha;
   const x = event.beta;
   const y = event.gamma;
 
-  z_dir.textContent = `z : ${z.toFixed(2)}\n`;
   x_dir.textContent = `x : ${x.toFixed(2)}\n`;
   y_dir.textContent = `y : ${y.toFixed(2)}\n`;
+  x += 90;
+  y += 90;
+
+  player.style.top = `${(maxY * y) / 180 - 10}px`;
+  player.style.left = `${(maxX * x) / 180 - 10}px`;
 }
 
 window.addEventListener("deviceorientation", orientering);
