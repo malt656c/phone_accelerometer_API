8a697a5e4b0298b1539bdb1c7e2a57b36a3ef8bd
Malte Jensen <malt656c@stud.kea.dk>
2023-01-22 21:12:53 +0100

    ge

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const x_dir = document.querySelector(".x-direction");
 const y_dir = document.querySelector(".y-direction");
 
 function orientering(event) {
-  const alpha = event.alpha;
-  const beta = event.beta;
-  const gamma = event.gamma;
-  z_dir.textContent = alpha;
-  x_dir.textContent = beta;
-  y_dir.textContent = gamma;
+  const z = event.alpha;
+  const x = event.beta;
+  const y = event.gamma;
+
+  z_dir.textContent = `z : ${z}\n`;
+  x_dir.textContent = `x : ${x}\n`;
+  y_dir.textContent = `y : ${y}\n`;
 }
+
 window.addEventListener("deviceorientation", orientering);
