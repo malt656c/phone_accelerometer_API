cda5917cddd969fc1bbddfe1dac7e2135a2d2c4c
Malte Jensen <malt656c@stud.kea.dk>
2023-01-23 20:22:42 +0100

    beta

--- script.js
+++ script.js
@@ -5,30 +5,45 @@ const player = document.querySelector(".player");
 
 const maxX = arena.clientWidth - player.clientWidth;
 const maxY = arena.clientHeight - player.clientHeight;
+let currentY = 0;
+let currentX = 0;
+let speedY = 0;
+let speedX = 0;
+window.addEventListener("deviceorientation", orientering);
 
 function orientering(event) {
-  const x = event.beta;
-  const y = event.gamma;
+  let x = event.beta;
+  let y = event.gamma;
+  let gravityX = y / 500;
+  let gravityY = x / 500;
 
-  if (y < 0) {
-    player.style.left = `-10px`;
+  interval = setInterval(newPos(), 50);
+  clearInterval(interval);
+  function newPos() {
+    speedY += gravityY;
+    speedX += gravityX;
+    currentY += speedY;
+    currentX += speedX;
   }
-  if (y > 0) {
-    player.style.left = `${maxX - 10}px`;
+  if (currentY > maxY-10) {
+    currentY = maxY-10;
+    speedY = 0;
+  }
+  if (currentY < 10) {
+    currentY = 10;
+    speedY = 0;
   }
 
-  if (x < 0) {
-    player.style.top = `-10px`;
+  if (currentX > maxX-10) {
+    currentX = maxX-10;
+    speedX = 0;
   }
-  if (x > 0) {
-    player.style.top = `${maxY - 10}px`;
+  if (currentX < -10) {
+    currentX = -10;
+    speedX = 0;
   }
-
-/*   player.style.transitionDuration = `${Math.abs(10/y)}s, ${Math.abs(10/x)}s` */
-
-  x_dir.textContent = `x : ${x.toFixed(2)} ${maxX}`;
-  y_dir.textContent = `y : ${y.toFixed(2)} ${maxY}`;
-
+  player.style.top = `${currentY}px`;
+  player.style.left = `${currentX}px`;
+  x_dir.textContent = `x : ${x.toFixed(2)} ${maxX} ${gravityY.toFixed(2)} ${currentY.toFixed(2)}`;
+  y_dir.textContent = `y : ${y.toFixed(2)} ${maxY} ${gravityX.toFixed(2)} ${currentX.toFixed(2)}`;
 }
-
-window.addEventListener("deviceorientation", orientering);
