e2921d507d970a55d1637a6cdc715e7926d17f98
Malte Jensen <malt656c@stud.kea.dk>
2023-01-23 02:13:04 +0100

    asd

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const x_dir = document.querySelector(".x-direction");
 const y_dir = document.querySelector(".y-direction");
 const arena = document.querySelector(".arena");
@@ -7,16 +6,29 @@ const player = document.querySelector(".player");
 const maxX = arena.clientWidth - player.clientWidth;
 const maxY = arena.clientHeight - player.clientHeight;
 
-console.log(maxX, maxY)
 function orientering(event) {
-  const x = event.beta+90;
-  const y = event.gamma+90;
+  const x = event.beta;
+  const y = event.gamma;
 
-  player.style.top = `${(maxX * x) / 90 - 10}px`;
-  player.style.left = `${(maxY * y) / 360 - 10}px`;
+  if (y < 0) {
+    player.style.left = `-10px`;
+  }
+  if (y > 0) {
+    player.style.left = `${maxX - 10}px`;
+  }
 
-   x_dir.textContent = `x : ${x.toFixed(2)} ${maxX}`;
- y_dir.textContent = `y : ${y.toFixed(2)} ${maxY}`;
+  if (x < 0) {
+    player.style.top = `-10px`;
+  }
+  if (x > 0) {
+    player.style.top = `${maxY - 10}px`;
+  }
+
+/*   player.style.transitionDuration = `${Math.abs(10/y)}s, ${Math.abs(10/x)}s` */
+
+  x_dir.textContent = `x : ${x.toFixed(2)} ${maxX}`;
+  y_dir.textContent = `y : ${y.toFixed(2)} ${maxY}`;
+
 }
 
 window.addEventListener("deviceorientation", orientering);
